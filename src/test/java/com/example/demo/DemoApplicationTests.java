ea368e9e69137ef635e5aba0a0fbb7a53dc64d83
watri <chieewhatt@gmail.com>
2024-02-01 20:16:11 +0700

    edit test coverage

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,15 +1,32 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DemoApplicationTests {
+
+	@Autowired
+    private MockMvc mockMvc;
+
 
 	@Test
 	void contextLoads() {
 		// This is a placeholder test method. Application context loading is implicitly tested
     	// by the @SpringBootTest annotation. Add specific tests for your components as needed.
 	}
+
+	@Test
+	public void testHomeEndpoint() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+               .andExpect(MockMvcResultMatchers.status().isOk())
+               .andExpect(MockMvcResultMatchers.content().string("Hello World! Watri Wahab Full Test"));
+    }
 
 }
